83d501544f6a7bda8716c6ccdcbd072cc6af8625
Adam Bausch <Fireworksanimations21@gmail.com>
2023-09-26 11:37:32 +0200

    t

--- IntegratedGameplaySystems/Assets/Scripts/GameManager.cs
+++ IntegratedGameplaySystems/Assets/Scripts/GameManager.cs
@@ -32,8 +32,23 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //ZORG DAT DIT BOVENAAN STAAT ANDERS KRIJG JE EEN NULLREFERENCE
+        fsm = new FSM<GameManager>();
+        fsm.Initialize(this);
+        inputHandler = new InputHandler();
+
+        var playerMovement = new PlayerMovement(fsm);
         var fireGun = new FireGunCommand(fsm);
         inputHandler.BindInputToCommand(KeyCode.X, fireGun, new MovementContext { Direction = Vector3.up });
+        inputHandler.BindInputToCommand(KeyCode.W, playerMovement, new MovementContext { Direction = Vector3.up });
+
+
+        fsm.AddState(new InstantiateGameObjects(fsm));
+        fsm.AddState(fireGun);
+        fsm.AddState(new IdleState(fsm));
+        fsm.AddState(playerMovement);
+
+        //fsm.SwitchState(typeof(InstantiateGameObjects));
     }
 
     // Update is called once per frame
